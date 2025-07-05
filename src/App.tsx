2dffa06f167bbd74e6d036f7a5cad702eec2cf7c
waiyanhtet1 <waiyanhtet1661390@gmail.com>
2025-07-05 23:51:36 +0630

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App = () => {
   const userInfo = getLoginUser();
 
   const [isUserNameUpdateModalOpen, setIsUserNameUpdateModalOpen] =
-    useState(true);
+    useState(false);
 
   useEffect(() => {
     if (userInfo && userInfo.username === "AppleUser") {
